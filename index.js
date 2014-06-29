b063e3c2496f956611250b16eca32cdedc27b0ee
Cnetwork <shuguang.wang1990@gmail.com>
2014-06-29 20:37:14 +0800

    warmup 4 complete

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var CTOR_NAME = 'initialize'
-var Class = function(properties) {
-	function ctor() {
-		if (properties.hasOwnProperty(CTOR_NAME)) {
-			properties[CTOR_NAME].apply(this, arguments);
+var CTOR_NAME = "initialize";
+var Class = function(properties){
+	function ctor(){
+		if(properties.hasOwnProperty(CTOR_NAME)){
+			properties[CTOR_NAME].apply(this,arguments);
 		}
 	}
+	for (var key in properties){
+		if(!properties.hasOwnProperty(key)){
+			continue;
+		}
+		if(key === CTOR_NAME){
+			continue;
+		}
 
-	for (var key in properties) {
-		if(!properties.hasOwnProperty(key)) {
-			continue;
-		}
-		if (key === CTOR_NAME) {
-			continue;
-		}
 		ctor.prototype[key] = properties[key];
 	}
-	
 	return ctor;
 };
 
---
+++ liuindex.js
@@ -0,0 +1,23 @@
+var CTOR_NAME = 'initialize'
+var Class = function(properties) {
+	function ctor() {
+		if (properties.hasOwnProperty(CTOR_NAME)) {
+			properties[CTOR_NAME].apply(this, arguments);
+		}
+	}
+	for (var key in properties) {
+		console.log(key);
+		if(!properties.hasOwnProperty(key)) {
+			continue;
+		}
+		if (key === CTOR_NAME) {
+			continue;
+		}
+		ctor.prototype[key] = properties[key];
+	}
+	
+	return ctor;
+};
+
+module.exports = Class;
+
